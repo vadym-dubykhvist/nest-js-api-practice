a723dff85c5292e5991eba372c4828580df125e3
vadym-dubykhvist <79746411+vadym-dubykhvist@users.noreply.github.com>
2026-05-07 17:32:34 +0200

    Add Prettier config, nodemon, alias, and Tag module
    Update tsconfig paths, add Tag controller/service, adjust imports to use
    @app alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { TagModule } from '@app/tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+if (!process.env.IS_TS_NODE) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('module-alias/register');
+}
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
